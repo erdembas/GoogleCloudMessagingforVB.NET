49e471244393c2bfa0b4a11373c0459c3fce00b5
Erdem Baş <erdembas64@gmail.com>
2013-03-01 22:02:02 +0200

    Server tarafında model oluşturuldu
    Model ve api controller oluşturuldu. Android cihazdan bir istek geldiğin
    REG_ID yi veritabanına kaydetmek için gerekli olan api düzenlendi...

---
+++ GCMServer/GCMServer/Controllers/ValueController.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Net
+Imports System.Web.Http
+
+Public Class ValueController
+    Inherits ApiController
+    Dim _Context As New GCMDatabaseEntities
+    ' GET api/value
+    Public Function GetValues() As IEnumerable(Of String)
+        Return New String() {"value1", "value2"}
+    End Function
+
+    ' GET api/value/5
+    Public Function GetValue(ByVal id As Integer) As String
+        Return "value"
+    End Function
+
+    ' POST api/value
+    Public Function PostValue(ByVal value As Kullanici) As String
+        Try
+            _Context.Kullanicis.Add(value)
+            _Context.SaveChanges()
+            Return "Başarılı"
+        Catch ex As Exception
+            Return "Bir sorun oluştu daha sonra  tekrar deneyiniz"
+        End Try
+    End Function
+
+    ' PUT api/value/5
+    Public Sub PutValue(ByVal id As Integer, <FromBody()> ByVal value As String)
+
+    End Sub
+
+    ' DELETE api/value/5
+    Public Sub DeleteValue(ByVal id As Integer)
+
+    End Sub
+End Class
